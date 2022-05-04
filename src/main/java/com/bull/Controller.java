de4253af249ca61896f41c4acc6337f574406d51
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-05-04 19:18:05 +0300

    Initial commit

---
+++ src/main/java/com/bull/Controller.java
@@ -0,0 +1,13 @@
+package com.bull;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/offerView")
+    public String page(){
+        return "html/offer";
+    }
+}
